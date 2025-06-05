878b00f2b91704b11d5b32d98f2c4e3c5db59bcc
Klas Kalass <klas.r.kalass@googlemail.com>
2025-06-05 14:31:58 +0200

    encoding/decoding tests for actually generated mappers

---
+++ test/mappers/annotation_mappers_test.dart
@@ -0,0 +1,63 @@
+import 'package:rdf_mapper/rdf_mapper.dart';
+import 'package:test/test.dart';
+
+// Import test models
+import '../fixtures/annotation_test_models.dart';
+// Import the generated init function
+import '../init_test_rdf_mapper.g.dart';
+
+void main() {
+  late RdfMapper mapper;
+  const baseUri = 'http://example.org/';
+
+  setUp(() {
+    mapper = initTestRdfMapper(
+      baseUriProvider: () => baseUri,
+    );
+  });
+
+  group('All Mappers Test', () {
+    test('BookWithMapper mapping', () {
+      final book = BookWithMapper(
+        id: '123',
+        title: 'Test Book',
+      );
+
+      // Test serialization
+      final graph = mapper.encodeObject(book);
+      expect(graph, isNotNull);
+
+      // Test deserialization
+      final deserialized = mapper.decodeObject<BookWithMapper>(graph);
+      expect(deserialized, isNotNull);
+      expect(deserialized.id, equals(book.id));
+      expect(deserialized.title, equals(book.title));
+    });
+
+    test('BookWithMapperInstance mapping', () {
+      final book = BookWithMapperInstance('456');
+
+      // Test serialization
+      final graph = mapper.encodeObject(book);
+      expect(graph, isNotNull);
+
+      // Test deserialization
+      final deserialized = mapper.decodeObject<BookWithMapperInstance>(graph);
+      expect(deserialized, isNotNull);
+      expect(deserialized.id, equals(book.id));
+    });
+
+    test('BookWithTemplate mapping', () {
+      final book = BookWithTemplate('789');
+
+      // Test serialization
+      final graph = mapper.encodeObject(book);
+      expect(graph, isNotNull);
+
+      // Test deserialization
+      final deserialized = mapper.decodeObject<BookWithTemplate>(graph);
+      expect(deserialized, isNotNull);
+      expect(deserialized.id, equals(book.id));
+    });
+  });
+}
---
+++ test/mappers/global_resource_processor_mappers_test.dart
@@ -0,0 +1,92 @@
+import 'package:rdf_mapper/rdf_mapper.dart';
+import 'package:test/test.dart';
+
+// Import test models
+import '../fixtures/global_resource_processor_test_models.dart';
+// Import the generated init function
+import '../init_test_rdf_mapper.g.dart';
+
+void main() {
+  late RdfMapper mapper;
+  const baseUri = 'http://example.org/';
+
+  setUp(() {
+    mapper = initTestRdfMapper(
+      baseUriProvider: () => baseUri,
+    );
+  });
+
+  group('All Mappers Test', () {
+    test('Book mapping', () {
+      final book = Book(
+        isbn: '1234567890',
+        title: 'Test Book',
+        authorId: 'author123',
+      );
+
+      // Test serialization
+      final graph = mapper.encodeObject(book);
+      expect(graph, isNotNull);
+
+      // Test deserialization
+      final deserialized = mapper.decodeObject<Book>(graph);
+      expect(deserialized, isNotNull);
+      expect(deserialized.isbn, equals(book.isbn));
+      expect(deserialized.title, equals(book.title));
+      expect(deserialized.authorId, equals(book.authorId));
+    });
+
+    test('ClassWithIriTemplateStrategy mapping', () {
+      final instance = ClassWithIriTemplateStrategy(id: 'template-strategy');
+
+      // Test serialization
+      final graph = mapper.encodeObject(instance);
+      expect(graph, isNotNull);
+
+      // Test deserialization
+      final deserialized =
+          mapper.decodeObject<ClassWithIriTemplateStrategy>(graph);
+      expect(deserialized, isNotNull);
+      expect(deserialized.id, equals(instance.id));
+    });
+
+    test('ClassWithIriTemplateAndContextVariableStrategy mapping', () {
+      final instance =
+          ClassWithIriTemplateAndContextVariableStrategy(id: 'context-var');
+
+      // Test serialization
+      final graph = mapper.encodeObject(instance);
+      expect(graph, isNotNull);
+
+      // Test deserialization
+      final deserialized = mapper
+          .decodeObject<ClassWithIriTemplateAndContextVariableStrategy>(graph);
+      expect(deserialized, isNotNull);
+      expect(deserialized.id, equals(instance.id));
+    });
+
+    test('ClassWithEmptyIriStrategy mapping', () {
+      final instance = ClassWithEmptyIriStrategy();
+      final graph = mapper.encodeObject(instance);
+      expect(graph, isNotNull);
+    });
+
+    test('ClassWithIriNamedMapperStrategy mapping', () {
+      final instance = ClassWithIriNamedMapperStrategy();
+      final graph = mapper.encodeObject(instance);
+      expect(graph, isNotNull);
+    });
+
+    test('ClassWithIriMapperStrategy mapping', () {
+      final instance = ClassWithIriMapperStrategy();
+      final graph = mapper.encodeObject(instance);
+      expect(graph, isNotNull);
+    });
+
+    test('ClassWithIriMapperInstanceStrategy mapping', () {
+      final instance = ClassWithIriMapperInstanceStrategy();
+      final graph = mapper.encodeObject(instance);
+      expect(graph, isNotNull);
+    });
+  });
+}
